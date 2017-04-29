9c0bba33859c1fbeaebed9fec6c8ba018aeded2e
Shehryar <xshehryar@gmail.com>
2017-04-29 04:48:43 -0400

    Add files via upload

---
+++ tokenize.py
@@ -0,0 +1,7 @@
+def input_file_lines(input_text, tokens):
+	tokens = input_text.splitlines();
+	return tokens
+
+def input_file_words(input_text, tokens):
+	tokens = input_text.split();
+	return tokens
